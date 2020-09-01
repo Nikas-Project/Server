71c3f3f68e5f8c5db3ce9f121790a3c4d108ebf0
Arash Hatami <hatamiarash7@gmail.com>
2020-09-01 17:20:20 +0430

    Update setup.py
    - Inc version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nikas',
-    version='1.0.1',
+    version='1.1.0',
     author='Arash Hatami',
     author_email='hatamiarash7@gmail.com',
     packages=find_packages(),
