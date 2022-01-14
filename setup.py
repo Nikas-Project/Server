35fcfa063f87ff78ea76f1e5ebc9ab02f04f961d
Arash Hatami <hatamiarash7@gmail.com>
2022-01-14 13:12:35 +0330

    increase version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nikas',
-    version='1.1.1',
+    version='1.1.2',
     author='Arash Hatami',
     author_email='hatamiarash7@gmail.com',
     packages=find_packages(),
