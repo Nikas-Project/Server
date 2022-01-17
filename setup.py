5f0231a0517e191d3ccf36ca3465f6fdd4c7f15f
Arash Hatami <hatamiarash7@gmail.com>
2022-01-17 17:30:25 +0330

    inc version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nikas',
-    version='1.1.8',
+    version='1.1.9',
     author='Arash Hatami',
     author_email='hatamiarash7@gmail.com',
     packages=find_packages(),
