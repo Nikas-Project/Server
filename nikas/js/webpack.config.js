510f2d813fc6f1fe0d1b7bbad141d659ba7db3c5
Arash Hatami <hatamiarash7@gmail.com>
2022-04-22 13:22:55 +0430

    Update webpack.config.js

--- nikas/js/webpack.config.js
+++ nikas/js/webpack.config.js
@@ -7,35 +7,23 @@ module.exports = [
             embed: path.resolve(__dirname, "embed.js"),
             count: path.resolve(__dirname, "count.js"),
         },
-        /* https://webpack.js.org/configuration/mode/
-         * Available modes: development, production, none
-         */
         mode: "development",
-        /* https://webpack.js.org/configuration/devtool/ */
         devtool: "source-map",
-        /* Instruct webpack to emit ES5-compatible syntax for not-so-recent (pre-2017) browsers
-         * Note: Both 'web' and 'es5' are needed!
-         * https://webpack.js.org/configuration/target/ */
         target: ["web", "es5"],
-        /* https://webpack.js.org/configuration/resolve/#resolvemodules */
         resolve: {
-            /* Allow omission of `nikas/js` path */
             modules: [
-                path.resolve(__dirname), // resolves to webpack.config.js file location
+                path.resolve(__dirname),
                 "node_modules",
             ],
         },
-        /* https://webpack.js.org/guides/asset-modules/ */
         module: {
             rules: [
                 {
-                    /* Read raw file contents when `require`-ing .svg files */
                     test: /\.svg/,
                     type: "asset/source",
                 },
             ],
         },
-        /* https://webpack.js.org/concepts/output/ */
         output: {
             filename: "[name].dev.js",
             path: path.resolve(__dirname),
@@ -43,24 +31,16 @@ module.exports = [
     },
     {
         name: "prod",
-        // https://webpack.js.org/configuration/configuration-types/#dependencies
         dependencies: ["dev"],
         mode: "production",
         optimization: {
-            /* Tree shaking
-             * https://webpack.js.org/guides/tree-shaking/ */
             usedExports: true,
-            // sideEffects=true, evaluates package.json sideEffects of inherited
-            // modules for tree-shaking of unused imports
-            // on by default for prod builds
-            //sideEffects: true,
         },
-        devtool: false, // no eval or source maps in prod
+        devtool: false,
         output: {
             filename: "[name].min.js",
             path: path.resolve(__dirname),
         },
     },
 ];
-// https://webpack.js.org/configuration/configuration-types/#parallelism
 module.exports.parallelism = 4;
